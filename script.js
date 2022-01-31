00dda473933927efc30e3ff0af88475761685ad7
Gabicold <biel18102004@gmail.com>
2022-01-31 14:45:28 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,34 @@
+//1 - Criar um loop que conte de 1 até 10 usando FOR
+console.log("1 - Criar um loop que conte de 1 até 10 usando FOR")
+
+for (let number = 0; number <= 10; number++) {
+    console.log(number);
+    
+ }
+//2 - Criar um loop que conte de 10 até 1 usando WHILE
+ console.log("2 - Criar um loop que conte de 10 até 1 usando WHILE")
+
+
+let contagem = 10
+while(contagem >= 1){
+    console.log(contagem)
+    contagem--
+}
+//3 - Criar um loop que conte todos os números ímpares de 1 até 100
+console.log("3 - Criar um loop que conte todos os números ímpares de 1 até 100")
+
+let contagem2 = 1
+
+while(contagem2 <= 100){
+    console.log(contagem2)
+    contagem2 +=2
+}
+//4- Criar um loop que conte todos os números pares de 0 a 100
+console.log("4- Criar um loop que conte todos os números pares de 0 a 100")
+
+let contagem3 = 0
+
+while(contagem3 <= 100){
+    console.log(contagem3)
+    contagem3 +=2
+}
